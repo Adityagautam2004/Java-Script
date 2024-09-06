1a343ca89f6f428f3070967a5dd6909dda5e3195
Adityagautam2004 <145846812+Adityagautam2004@users.noreply.github.com>
2024-09-06 15:02:15 +0000

    Data Types And Variables

---
+++ Basics/Variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553
+let accountEmail = "adikr2002@gmaill.com" 
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+//accountId = 43
+accountEmail="adikr20022@gmail.com"
+accountPassword="67577"
+accountCity="Patna"
+/*
+Prefer Not to use Var
+because of issue in block scope and functional scope
+*/
+
+
+console.log(accountId)
+console.log(accountEmail)
+console.log(accountPassword)
+console.log(accountCity)
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ Basics/data_types.js
@@ -0,0 +1,19 @@
+"use Strict"; //treat all js code as newer version
+//alert(3+3)// we are using node js , not browser
+
+
+let name ="Aditya"
+let age = 18
+let isLoggedIn= false
+
+
+// Number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone Value
+//undefined =>
+// symbol => unique
+
+//object
+console.log(typeof name);
