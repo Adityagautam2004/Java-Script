5f0d30585c1f2c7ef9026f2b35ff58e924d73a1a
Adityagautam2004 <145846812+Adityagautam2004@users.noreply.github.com>
2024-09-08 13:50:54 +0000

    conversion of data types

---
+++ Basics/ConversionOperations.js
@@ -0,0 +1,20 @@
+let Score = null
+console.log(typeof Score)
+console.log(typeof (Score))
+
+let valueInNumber = Number (Score)
+console.log(typeof Score)
+console.log( Score)
+
+//"33" => 33
+//"33abc" => NaN
+// true => 1 ; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1=> true ; 0 => false
+// " " => false
+// "aditya" => true
