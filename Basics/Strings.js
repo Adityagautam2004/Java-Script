168f0abfac50abbb3c436d4589e478928dd52173
Adityagautam2004 <145846812+Adityagautam2004@users.noreply.github.com>
2024-09-10 12:38:08 +0000

    strings done

--- Basics/Strings.js
+++ Basics/Strings.js
@@ -1,5 +1,31 @@
 const name= "Aditya"
 const sirname =" Gautam"
 
-console.log(name + sirname)
-console.log(`Shubham  bhadwa sala randibaaz`)
+//console.log(name + sirname)
+//console.log(`Shubham ${name} ${sirname}bhadwa sala randibaaz`)
+
+const gameName = new String ('FortuneFirst')
+
+console.log(gameName[0])
+console.log(gameName.__proto__)
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(2))
+console.log(gameName.indexOf('e'))
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   hitesh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
