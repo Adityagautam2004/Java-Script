daaa6f74cef96101dfd16c8f3a690f50ef8eb4a5
Adityagautam2004 <145846812+Adityagautam2004@users.noreply.github.com>
2024-09-18 15:11:20 +0000

    objects Start

---
+++ Basics2/objects.js
@@ -0,0 +1,15 @@
+// singleton
+// Object.create
+
+// object literals
+const JsUser ={
+    name: "Aditya",
+    "full name" :"Aditya Gautam",
+    location : 21 ,
+    email :"adikr2002@gmail.com",
+    isLoggedIn : false,
+    lastLoginDays : ["Monday", "Saturday"]
+}
+
+console.log(JsUser.email)
+console.log(JsUser["email"])
