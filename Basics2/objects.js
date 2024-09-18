3fdb9ba7df507237539d38ed60f71b7bc4c2ffbc
Adityagautam2004 <145846812+Adityagautam2004@users.noreply.github.com>
2024-09-18 20:08:55 +0000

    objects

--- Basics2/objects.js
+++ Basics2/objects.js
@@ -2,9 +2,12 @@
 // Object.create
 
 // object literals
+
+const mySym = Symbol("key1")
 const JsUser ={
     name: "Aditya",
     "full name" :"Aditya Gautam",
+    [mySym]: "key1",
     location : 21 ,
     email :"adikr2002@gmail.com",
     isLoggedIn : false,
@@ -13,3 +16,20 @@ const JsUser ={
 
 console.log(JsUser.email)
 console.log(JsUser["email"])
+console.log(JsUser[mySym])
+
+
+JsUser.email = "hitesh@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "hitesh@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
